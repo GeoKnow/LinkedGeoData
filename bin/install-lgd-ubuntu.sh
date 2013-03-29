e351956024e7c5c6f0e0b34b8e16cb5539ec3fdb
cstadler <cstadler@informatik.uni-leipzig.de>
2013-03-29 21:52:25 +0100

    Added git and maven to the dependencies

--- bin/install-lgd-ubuntu.sh
+++ bin/install-lgd-ubuntu.sh
@@ -2,5 +2,5 @@
 #
 # Installs the required Ubuntu system packages for running LinkedGeoData
 #
-sudo apt-get install postgresql postgresql-contrib-9.1 postgis postgresql-9.1-postgis osmosis
+sudo apt-get install postgresql postgresql-contrib-9.1 postgis postgresql-9.1-postgis osmosis git maven
 
