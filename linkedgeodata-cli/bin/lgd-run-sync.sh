73b04af596f63c3368904f9b0148a6190f4b168e
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2017-06-26 18:46:38 +0200

    updated sync wrapper

--- linkedgeodata-cli/bin/lgd-run-sync.sh
+++ linkedgeodata-cli/bin/lgd-run-sync.sh
@@ -7,33 +7,21 @@ osmosis="osmosis"
 targetPath="sync"
 sleepInterval="60"
 
-m_info()
-{
-	echo "[`date +"%Y-%m-%d %H:%M:%S"`] $$ $1"
-}
-
-m_error()
-{
-	echo "[`date +"%Y-%m-%d %H:%M:%S"`] $$ [error] $1"
-	exit 1
-}
-
-m_ok()
-{
-	echo "[`date +"%Y-%m-%d %H:%M:%S"`] $$ $1"
-}
-
+log() { echo "[`date +"%Y-%m-%d %H:%M:%S"`] $$ [$1] $2" }
 
 echo "Starting LinkedGeoData live update (based on osmosis)..."
 
 
-if [ ! -f $configFileName ]; then
-	m_info "No config found, attempting to copy '$configFileName.dist' to '$configFileName'"
-	cp "$configFileName.dist" $configFileName
-fi
+if [ ! -z "configFileName" !; then
+	if [ ! -f "$configFileName" ]; then
+		log "info" "No config found, attempting to copy '$configFileName.dist' to '$configFileName'"
+		cp "$configFileName.dist" "$configFileName"
+	fi
 
-if ! source $configFileName; then
-	m_error "Could not load config file: '$configFileName'"
+	if ! source "$configFileName"; then
+		log "error" "Could not load config file: '$configFileName'"
+		exit 1
+	fi
 fi
 
 mkdir -p "$targetPath"
@@ -45,22 +33,23 @@ then
 	"$osmosis" --read-replication-interval-init workingDirectory="$targetPath"
 
 	if [ "$?" -eq "0" ]; then	
-		m_info "Osmosis target directory has been initialized. Please check configuration in $targetPath, then run this script again"
+		log "info" "Osmosis target directory has been initialized. Please check configuration in $targetPath, then run this script again"
 	else
-		m_error "Osmosis exited with error code ($?)"
+		log "error" "Osmosis exited with error code ($?)"
+		exit 1
 	fi
 	exit 0
 fi
 
 
 function applyChanges {
-    h="$dbHost"
-    if [ ! -z "$dbPort" ]; then
-        h="$dbHost:$dbPort"
+    h="$DB_HOST"
+    if [ ! -z "$DB_PORT" ]; then
+        h="$DB_HOST:$DB_PORT"
     fi
 
     if [ -f "$targetPath/diff.osc" ]; then
-        "$osmosis" --read-xml-change file="$targetPath/diff.osc" --write-pgsimp-change host="$dbHost" database="$dbName" user="$dbUser" password="$dbPass"
+        "$osmosis" --read-xml-change file="$targetPath/diff.osc" --write-pgsimp-change host="$h" database="$DB_NAME" user="$DB_USER" password="$DB_PASS"
 
         # TODO Decide whether the old diffs should be kept or removed
         # Remove possibly existing diff.osc file
