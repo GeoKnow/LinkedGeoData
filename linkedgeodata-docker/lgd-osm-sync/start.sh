20eedee0c8f02910b35ac73ba07371eefcc04dac
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2017-06-27 02:22:50 +0200

    sync finally starting

--- linkedgeodata-docker/lgd-osm-sync/start.sh
+++ linkedgeodata-docker/lgd-osm-sync/start.sh
@@ -15,11 +15,16 @@ statusVal=`psql "$DB_URL" -tc "SELECT \"v\" FROM \"status\" WHERE "k"='$statusKe
 
 echo "Retrieved status value from $DB_URL for key [$statusKey] is '$statusVal'"
 
+mkdir -p sync
+
 # If the status is empty, then load the data
 if [ -z "$statusVal" ]; then
   #PBF_DATA=http://downloads.linkedgeodata.org/debugging/monaco-170618.osm.pbf
   rm -f data.osm.pbf
   curl -L "$OSM_DATA_BASE_URL" --create-dirs -o data.osm.pbf
+
+  timestamp=`osmconvert --out-timestamp "data.osm.pbf"`
+  curl "https://osm.mazdermind.de/replicate-sequences/?$timestamp" > sync/state.txt
 
 
 # TODO Fix lgd-createdb to include port
@@ -29,8 +34,9 @@ if [ -z "$statusVal" ]; then
   psql "$DB_URL" -c "INSERT INTO \"status\"(\"k\", \"v\") VALUES('$statusKey', 'loaded')"
 fi
 
+# NOTE lgd-run-sync uses ./sync/configuration.txt by default
 
-cat configuration.txt.dist | envsubst > configuration.txt
+cat configuration.txt.dist | envsubst > sync/configuration.txt
 
 # The sync script picks up the environment variables
 /usr/share/linkedgeodata/lgd-run-sync.sh
