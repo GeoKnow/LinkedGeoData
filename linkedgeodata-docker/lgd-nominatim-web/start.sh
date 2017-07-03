dfb0daf388f3f6704fc9f88c892902e580dc1e93
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2017-07-03 17:57:41 +0200

    fixed nominatim web interface

--- linkedgeodata-docker/lgd-nominatim-web/start.sh
+++ linkedgeodata-docker/lgd-nominatim-web/start.sh
@@ -5,8 +5,5 @@ cat ./src/settings/local.php.dist | envsubst > ./src/settings/local.php
 rm -rf /var/www/html/*
 ./src/utils/setup.php --create-website /var/www/html
 
-# Apache configure
-COPY nominatim.conf /etc/apache2/sites-enabled/000-default.conf
-
 /usr/sbin/apache2ctl -D "FOREGROUND"
 
