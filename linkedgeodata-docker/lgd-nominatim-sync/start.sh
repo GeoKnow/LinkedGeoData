b9df09a081fa52125a0fe015913607dda3174123
Claus Stadler <RavenArkadon@googlemail.com>
2017-10-02 23:26:58 +0200

    enhanced dockerization to allow disabling of data replication

--- linkedgeodata-docker/lgd-nominatim-sync/start.sh
+++ linkedgeodata-docker/lgd-nominatim-sync/start.sh
@@ -10,6 +10,12 @@ echo "lgd-osm-sync environment:"
 env | grep -i "osm\|db\|post"
 
 
+if [ -z "$OSM_DATA_SYNC_URL" ]; then
+	echo "Note: Data replication disabled as OSM_DATA_SYNC_URL no set"
+	exit 0
+fi
+
+
 # Check the database whether the data was loaded
 statusKey="nominatim:status"
 
