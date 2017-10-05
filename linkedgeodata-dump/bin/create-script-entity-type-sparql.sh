422344128777d117b621f8d03b3725ae5fefc35c
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2017-10-05 15:17:48 +0200

    dump now accepts sparql url as argument

---
+++ linkedgeodata-dump/bin/create-script-entity-type-sparql.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echoerr() { echo "$@" 1>&2; }
+
+typ="$1"
+entity="$2"
+options="$3"
+folder="$4"
+path="$5"
+
+[ -z "$typ" ] && echo "No class provided (e.g. Shop)"
+[ -z "$entity" ] && echo "No entity type provided (node, way or relation)"
+[ -z "$folder" ] && folder=`date +%F`
+[ -z "$path" ] && path="../target/dump/"
+
+targetDir="$path/$folder/"
+mkdir -p "$targetDir"
+
+#echoerr "Settings: $options | $folder | $path"
+
+uri="http://linkedgeodata.org/ontology/$typ"
+file="$folder-$typ.$entity.sorted.nt.bz2"
+
+tempfile=`tempfile`
+
+"./create-queries-${entity}s.sh" "$uri" | while read line; do
+#    echo "sparqlify-tool $options -Q '$line' | sort -u -S 1024M | rapper -i ntriples - http://example.org/ | pbzip2 -c > '$targetDir/$file'"
+#    echo "sparqlify-tool $options -Q '$line' >> '$tempfile'"
+    echo "curl -LH 'Accept: text/plain' -G --data-urlencode 'query=$line' $options >> '$tempfile'"
+done
+
+echo "cat $tempfile | sort -u -S 1024M | rapper -i ntriples - http://example.org/ | pbzip2 -c > '$targetDir/$file'"
+echo "rm '$tempfile'"
+
