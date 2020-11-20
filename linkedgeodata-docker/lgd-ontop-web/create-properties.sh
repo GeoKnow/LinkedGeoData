9761041d6c2667bd27a3f4644a5a1014b27c1548
Claus Stadler <RavenArkadon@googlemail.com>
2020-11-20 18:44:59 +0100

    More work on the ontop container

--- linkedgeodata-docker/lgd-ontop-web/create-properties.sh
+++ linkedgeodata-docker/lgd-ontop-web/create-properties.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 
 cat <<EOL
-jdbc.url=${ONTOP_DB_URL}
-#jdbc.driver=org.postgresql.Driver
-jdbc.driver=com.postgresql.jdbc.Driver
+jdbc.url=jdbc:${ONTOP_DB_URL}
+jdbc.driver=org.postgresql.Driver
+#jdbc.driver=com.postgresql.jdbc.driver
 jdbc.user=${ONTOP_DB_USER}
 jdbc.name=${ONTOP_DB_NAME}
 jdbc.password=${ONTOP_DB_PASSWORD}
