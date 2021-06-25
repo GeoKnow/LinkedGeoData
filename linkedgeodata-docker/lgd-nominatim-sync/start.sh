a291d9c2d0782ec9bf48419e91ac77808b2158e7
Claus Stadler <RavenArkadon@googlemail.com>
2021-06-25 09:28:05 +0200

    Updated nominatim start script

--- linkedgeodata-docker/lgd-nominatim-sync/start.sh
+++ linkedgeodata-docker/lgd-nominatim-sync/start.sh
@@ -10,8 +10,11 @@ echo "lgd-osm-sync environment:"
 env | grep -i "osm\|db\|post"
 
 
-echo "$DB_HOST:$DB_PORT:$DB_NAME:$DB_USER:$DB_PASS" > /root/.pgpass
-chmod 600 /root/.pgpass
+# Create the .pgpass file in the volume so that it can be inspected more easily
+echo "$DB_HOST:$DB_PORT:$DB_NAME:$DB_USER:$DB_PASS" > .pgpass
+chmod 600 .pgpass
+sleep 2
+mv /root/.pgpass
 
 
 # Check the database whether the data was loaded
