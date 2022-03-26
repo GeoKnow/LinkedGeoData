7d5739d4b2e4f9384721f93d9c03744b3b8a2066
Claus Stadler <RavenArkadon@googlemail.com>
2022-03-26 19:29:42 +0100

    More fiddling with the setup; towards making lgd-nominatim-build 'fat' trading a bit of disk space for much faster docker image builds

--- lgd-docker/2022/lgd-tools-parent/lgd-tools-cli/src/main/java/org/aksw/linkedgeodata/cli/command/main/VersionProviderLinkedGeoData.java
+++ lgd-docker/2022/lgd-tools-parent/lgd-tools-cli/src/main/java/org/aksw/linkedgeodata/cli/command/main/VersionProviderLinkedGeoData.java
@@ -9,8 +9,8 @@ import org.aksw.commons.picocli.VersionProviderFromClasspathProperties;
 public class VersionProviderLinkedGeoData
 	extends VersionProviderFromClasspathProperties
 {
-	@Override public String getResourceName() { return "linkedgeodata.properties"; }
+	@Override public String getResourceName() { return "lgd.properties"; }
 	@Override public Collection<String> getStrings(Properties p) { return Arrays.asList(
-        p.get("linkedgeodata.version") + " built at " + p.get("linkedgeodata.build.timestamp")); }
+        p.get("lgd.version") + " built at " + p.get("lgd.build.timestamp")); }
 
 }
